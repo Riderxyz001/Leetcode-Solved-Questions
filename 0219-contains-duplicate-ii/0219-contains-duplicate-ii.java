e4cb7d2c214012ff0219b7520ea6c967b6d99c74
AJAY YADAV <ajayyadav15062004@gmail.com>
2026-06-26 12:50:32 +0530

    Time: 1392 ms (5.03%), Space: 69.8 MB (98.56%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        for(int i = 0; i < nums.length; i++) {
+
+            for(int j = i + 1; j <= Math.min(i + k, nums.length - 1); j++) {
+
+                if(nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
